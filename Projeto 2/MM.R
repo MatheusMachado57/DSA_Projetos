5c2fb3d18231fc1ec0c9ddd39d0fa269f885325a
Matheus Machado de Almeida <40668494+MatheusMachado57@users.noreply.github.com>
2021-10-11 09:23:45 -0300

    Project2

---
+++ Projeto 2/MM.R
@@ -0,0 +1,13 @@
+
+# base = read.csv("test.csv")
+
+# set.seed(219054066)
+
+# Separação entre Treino e Teste
+# inTrain <- caret::createDataPartition(base$id, 
+#                                       p = 0.1, 
+#                                       list = F)
+
+# treino  <- base[ inTrain,]
+# write.csv(treino, "MM.csv")
+
